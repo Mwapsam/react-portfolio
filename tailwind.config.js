dc13f83ecad5caa313726db2e893a6cc9a949d2d
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-07-01 14:34:14 +0200

    Configure tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  model: 'jit',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-purple': '#1B3C4B',
+        'light-white': 'background: #FFFFFF',
+      },
+    },
+  },
+  plugins: [],
+};
