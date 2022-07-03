12b3343de81233bf2499ba55846fd0ef94515eda
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-07-03 17:43:59 +0200

    Create project data object file

---
+++ src/data/projects.js
@@ -0,0 +1,81 @@
+import books from '../assets/images/covid.png';
+import car from '../assets/images/car-app.jpg';
+import pokimon from '../assets/images/pokimon.png';
+import calculator from '../assets/images/calculator.png';
+
+const projects = [
+  {
+    name: 'Bookstore',
+    description: 'Work from home accessories',
+    imageSrc: books,
+    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+    href: '/bookstore-app',
+    techStack: ['React', 'Redux', 'Firebase', 'Styled Components', 'Material UI'],
+  },
+  {
+    name: 'Self-Improvement',
+    description: 'Journals and note-taking',
+    imageSrc: car,
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+    href: '/self-improvement',
+    techStack: ['React', 'Redux', 'JWT', 'Ruby', 'Ruby on Rails', 'PostgreSQL'],
+  },
+  {
+    name: 'Travel',
+    description: 'Daily commute essentials',
+    imageSrc: calculator,
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+    href: '/travel',
+    techStack: ['React', 'Redux', 'JavaScript', 'CSS', 'HTML'],
+  },
+  {
+    name: 'Bookstore',
+    description: 'Work from home accessories',
+    imageSrc: books,
+    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+    href: '/bookstore-app',
+    techStack: ['React', 'Redux', 'Firebase', 'Styled Components', 'Material UI'],
+  },
+  {
+    name: 'Self-Improvement',
+    description: 'Journals and note-taking',
+    imageSrc: car,
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+    href: '/self-improvement',
+    techStack: ['React', 'Redux', 'JWT', 'Ruby', 'Ruby on Rails', 'PostgreSQL'],
+  },
+  {
+    name: 'Travel',
+    description: 'Daily commute essentials',
+    imageSrc: pokimon,
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+    href: '/travel',
+    techStack: ['React', 'Redux', 'JavaScript', 'CSS', 'HTML'],
+  },
+  {
+    name: 'Bookstore',
+    description: 'Work from home accessories',
+    imageSrc: books,
+    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
+    href: '/bookstore-app',
+    techStack: ['React', 'Redux', 'Firebase', 'Styled Components', 'Material UI'],
+  },
+  {
+    name: 'Self-Improvement',
+    description: 'Journals and note-taking',
+    imageSrc: car,
+    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
+    href: '/self-improvement',
+    techStack: ['React', 'Redux', 'JWT', 'Ruby', 'Ruby on Rails', 'PostgreSQL'],
+  },
+  {
+    name: 'Travel',
+    description: 'Daily commute essentials',
+    imageSrc: pokimon,
+    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
+    href: '/travel',
+    techStack: ['React', 'Redux', 'JavaScript', 'CSS', 'HTML'],
+  },
+];
+
+export default projects;
